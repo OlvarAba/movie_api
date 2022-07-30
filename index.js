48860107fff9afb428848c6a795934d8af4d7455
Olvar Abadeen <oabadeen@gmail.com>
2022-07-29 19:05:56 -0700

    added models and query with mongoose

--- index.js
+++ index.js
@@ -1,64 +1,117 @@
+const mongoose = require('mongoose');
+const Models = require('./models.js');
+
+const Movies = Models.Movie;
+const Users = Models.User;
+mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
+
+
 const express = require('express'),
   morgan = require('morgan'),
-  bodyParser = require('body-parser'),
+  bodyParser = require('body-parser');
   uuid = require('uuid'),
   app = express();
-
- let users = [
- ]
-
- let movies = [
-   {
-    Title: 'Midsommar',
-  },
-  {
-    Title: 'Everything Everywhere, All at Once',
-  },
-  {
-    Title: 'The Lighthouse',
-  },
-  {
-    Title: 'Men',
-  },
-  {
-    Title: 'The House',
-  },
-  {
-    Title: 'All Dogs go to heaven 2',
-  },
-  {
-    Title: 'Too wong foo, Thanks for everything Julie Newmar',
-  },
-  {
-    Title: 'Pulp Fiction',
-  },
-  {
-    Title: 'Fantastic Mr Fox',
-  },
-  {
-    Title: 'Blood Diamond',
-  },
-
- ];
+  app = express();
 
   app.use(bodyParser.json());
+
+  app.use(bodyParser.urlencoded({ extended: true }));
 
   app.use(express.static('public'));
 
   app.use(morgan('common'));
 
- // CREATE
- app.post('/users', (req, res) => {
-   const newUser = req.body;
+  //Add a user
+/* We’ll expect JSON in this format
+{
+  ID: Integer,
+  Username: String,
+  Password: String,
+  Email: String,
+  Birthday: Date
+}*/
+app.post('/users', (req, res) => {
+  Users.findOne({ Username: req.body.Username })
+    .then((user) => {
+      if (user) {
+        return res.status(400).send(req.body.Username + 'already exists');
+      } else {
+        Users
+          .create({
+            Username: req.body.Username,
+            Password: req.body.Password,
+            Email: req.body.Email,
+            Birthday: req.body.Birthday
+          })
+          .then((user) =>{res.status(201).json(user) })
+        .catch((error) => {
+          console.error(error);
+          res.status(500).send('Error: ' + error);
+        })
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send('Error: ' + error);
+    });
+});
 
-   if (newUser.name) {
-     newUser.id = uuid.v4();
-     users.push(newUser);
-     res.status(201).json(newUser)
-   } else {
-     res.status(400).send('users need names.')
-   }
- })
+// Get all users
+app.get('/users', (req, res) => {
+  Users.find()
+    .then((users) => {
+      res.status(201).json(users);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
+});
+
+// Get a user by username
+app.get('/users/:Username', (req, res) => {
+  Users.findOne({ Username: req.params.Username })
+    .then((user) => {
+      res.json(user);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
+});
+
+// Update a user's info, by username
+/* We’ll expect JSON in this format
+{
+  Username: String,
+  (required)
+  Password: String,
+  (required)
+  Email: String,
+  (required)
+  Birthday: Date
+}*/
+app.put('/users/:Username', (req, res) => {
+  Users.findOneAndUpdate({ Username: req.params.Username }, { $set:
+    {
+      Username: req.body.Username,
+      Password: req.body.Password,
+      Email: req.body.Email,
+      Birthday: req.body.Birthday
+    }
+  },
+  { new: true }, // This line makes sure that the updated document is returned
+  (err, updatedUser) => {
+    if(err) {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    } else {
+      res.json(updatedUser);
+    }
+  });
+});
+
+
 
  // READ
  app.get('/movies', (req, res) => {
@@ -158,6 +211,9 @@ const express = require('express'),
    }
  })
 
+ app.get("/", (req, res) => {
+   res.send("Welcome To myFlix");
+ });
 
 
  app.get('/movies', (req, res) => {
