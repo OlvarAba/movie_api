2c7f375943092b1429930f78d462946000c6f435
Olvar Abadeen <oabadeen@gmail.com>
2022-09-16 19:06:55 -0700

    Update index.js

--- index.js
+++ index.js
@@ -10,22 +10,18 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("common"));
 const { check, validationResult } = require('express-validator');
 const cors = require("cors");
-const allowedOrigins = ["*"];
+let allowedOrigins = ['http://localhost:1234', 'http://localhost:8080', 'http://testsite.com'];
 
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        let message =
-          "The CORS policy for this application doesn’t allow access from origin " +
-          origin;
-        return callback(new Error(message), false);
-      }
-      return callback(null, true);
-    },
-  })
-);
+app.use(cors({
+  origin: (origin, callback) => {
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
+      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+      return callback(new Error(message ), false);
+    }
+    return callback(null, true);
+  }
+}));
 let auth = require('./auth')(app);
 const passport = require('passport');
 require('./passport');
